c8448e6afc56c327868d74fb90d1f21ea954fc3d
nicolastsypin <nicolastsypin23@gmail.com>
2025-05-08 20:48:18 -0400

    pipreqs fix

--- MLUnsupervisedApp/app.py
+++ MLUnsupervisedApp/app.py
@@ -1,7 +1,6 @@
 # importing necessary libraries, functions, and datasets
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_wine
